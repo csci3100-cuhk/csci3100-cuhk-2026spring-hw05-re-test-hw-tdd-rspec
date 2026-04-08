c21ba463a3441ca755a4a9bb080fd845b71bf98a
Shawdox <478956370@qq.com>
2026-04-08 19:40:10 +0800

    fix: add BigDecimal.new shim in boot for Ruby 2.7

--- config/boot.rb
+++ config/boot.rb
@@ -1,6 +1,16 @@
 require 'rubygems'
 
-# Set up gems listed in the Gemfile.
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+# Rails 4.2 expects BigDecimal.new, which is removed in newer Ruby.
+require 'bigdecimal'
+unless BigDecimal.respond_to?(:new)
+  class << BigDecimal
+    def new(*args)
+      Kernel.BigDecimal(*args)
+    end
+  end
+end
 
-require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
+# Set up gems listed in the Gemfile.
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
+
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
