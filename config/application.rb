b6c5ca48315be46b1613085a1324e48496da25d0
Shawdox <478956370@qq.com>
2026-04-08 19:22:41 +0800

    fix: add BigDecimal.new compatibility for Ruby 2.7

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,15 @@
-require File.expand_path('../boot', __FILE__)
+require File.expand_path('boot', __dir__)
+
+# Rails 4.2 calls BigDecimal.new internally. Ruby 2.7 ships BigDecimal
+# without .new, so add a compatibility shim before loading rails/all.
+require 'bigdecimal'
+unless BigDecimal.respond_to?(:new)
+  class << BigDecimal
+    def new(*args)
+      Kernel.BigDecimal(*args)
+    end
+  end
+end
 
 # # Pick the frameworks you want:
 # require "active_record/railtie"
@@ -7,7 +18,7 @@ require File.expand_path('../boot', __FILE__)
 # require "active_resource/railtie"
 # require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-require "rails/all"
+require 'rails/all'
 
 Bundler.require(:default, Rails.env)
 
@@ -36,13 +47,13 @@ module Rottenpotatoes
     # config.i18n.default_locale = :de
 
     # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
+    config.encoding = 'utf-8'
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
 
     # Enable the asset pipeline
-    #config.assets.enabled = true
+    # config.assets.enabled = true
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
@@ -60,10 +71,9 @@ module Rottenpotatoes
     # This will create an empty whitelist of attributes available for mass-assignment for all models
     # in your app. As such, your models will need to explicitly whitelist or blacklist accessible
     # parameters by using an attr_accessible or attr_protected declaration.
-    #config.active_record.whitelist_attributes = true
+    # config.active_record.whitelist_attributes = true
 
     # Fix for Heroku deploy
     config.assets.initialize_on_precompile = false
-
   end
 end
