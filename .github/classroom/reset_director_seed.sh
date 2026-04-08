ce006c2e29d2763c25b22e9489bd19323b8996c0
Shawdox <478956370@qq.com>
2026-04-08 19:17:33 +0800

    restore tmdb autograder workflow and fix ZenTest conflict

--- .github/classroom/reset_director_seed.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-
-RAILS_ENV=development bundle _1.15.3_ exec rails runner "Movie.delete_all; Movie.create!([{title: 'Star Wars', rating: 'PG', director: 'George Lucas', release_date: '1977-05-25', description: 'Space opera'}, {title: 'THX-1138', rating: 'R', director: 'George Lucas', release_date: '1971-03-11', description: 'Science fiction'}, {title: 'Blade Runner', rating: 'PG', director: 'Ridley Scott', release_date: '1982-06-25', description: 'Neo noir'}, {title: 'Alien', rating: 'R', director: nil, release_date: '1979-05-25', description: 'Space horror'}])"
